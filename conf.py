0a1711754de7618032c4ff4e3ac816c350def16f
Wolthera van Hövell tot Westerflier <griffinvalley@gmail.com>
2020-10-18 22:03:56 +0200

    Update manual cover and conf.py to 4.4.0

--- conf.py
+++ conf.py
@@ -42,9 +42,9 @@ except subprocess.CalledProcessError as exc:
     gitcommithash = release
 
 # The short X.Y version
-version = '4.3'
+version = '4.4'
 # The full version, including alpha/beta/rc tags
-release = '4.3.0'
+release = '4.4.0'
 
 
 # -- General configuration ---------------------------------------------------
