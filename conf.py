a6d24a3b96c7bd177525cf058b16ff716b59b591
Julius Künzel <julius.kuenzel@kde.org>
2025-01-10 14:19:18 +0100

    [config] No need to set
    According to the documentation a dict is the prefered type for this. However {'.rst': 'restructuredtext'} is already the default value so it is not required to overwrite it

--- conf.py
+++ conf.py
@@ -130,8 +130,10 @@ else:
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+# source_suffix = {
+#     '.rst': 'restructuredtext',
+#     '.md': 'markdown',
+# }
 
 # The master toctree document.
 master_doc = 'index'
